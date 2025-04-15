5474f00b9373488e71c17c30521cf6d05387129c
Keiliel Oliveira <keilieloliveira.2004@gmail.com>
2025-04-15 13:34:46 -0300

    Aprendendo Git
    Uso de branchs.
    
    git commands:
    git branch
    git checkout

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-msg(hello());
+msg(hello(includeName()));
 
-function hello() {
-    return "Hello World!";
+function hello(obj) {
+    return "Hello " + obj + "!";
 }
 
 function msg(msg) {
     console.log(msg);
 }
+
+function includeName(name = null) {
+    if(name == null) {
+        return "World"
+    }
+
+    return name.captalize();
+}
+
+Object.defineProperty(String.prototype, 'capitalize', {
+    value: function() {
+      return this.charAt(0).toUpperCase() + this.slice(1);
+    },
+    enumerable: false
+  });
